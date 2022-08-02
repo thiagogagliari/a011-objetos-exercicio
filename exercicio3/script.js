3e2463dcc1568fc1c0053cf396cb0f30507d1120
thiagogagliari <thiagogagliari@hotmail.com>
2022-08-02 18:39:02 -0300

    Resolução Exercício 011 Objetos

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -14,7 +14,12 @@ pokemon1.ataques = []
 // console.log(pokemon1)
 // console.log(pokemon1.ataques)
 
-pokemon1.ataques.push({nome: 'Investida'}, {dano: 48}, {tipo: 'Normal'}, {precisao: 100});
+pokemon1.ataques.push({
+    nome: 'Investida',
+    dano: 48, 
+    tipo: 'Normal',
+    precisao: 100});
+    
 console.log('Adicionando objetos com o push()', pokemon1)
 
 //c
@@ -22,11 +27,19 @@ pokemon2.ataques=[...pokemon1.ataques]
 console.log('Adicionando ataques ao segundo pokemon por spread', pokemon2)
 
 //d
-pokemon1.ataques.push({nome: 'Folha Navalha'}, {dano: 45}, {tipo: 'Grama'}, {precisao: 100})
+pokemon1.ataques.push({
+    nome: 'Folha Navalha',
+    dano: 45,
+    tipo: 'Grama',
+    precisao: 100})
 
 console.log('Adicionando outros ataques ao primeiro pokemon', pokemon1)
 
-pokemon2.ataques.push({nome: 'Jato de Água'}, {dano: 40}, {precisao: 100}, {tipo: 'Água'})
+pokemon2.ataques.push({
+    nome: 'Jato de Água',
+    dano: 40,
+    precisao: 100,
+    tipo: 'Água'})
 
 console.log('Assim ficou meu primeiro pokemon:', pokemon1)
 console.log('Assim ficou meu segundo pokemon:', pokemon2)
